053cca79a0e5f0e028ae9e94b9a090ed4cd3ec1a
Catequesis <juanprs29@gmail.com>
2025-12-13 02:19:44 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,11 @@ const CACHE_NAME = "biblia-365-v2";
 const FILES_TO_CACHE = [
   "/Plan_Lectura_Bilia_365_Dias/",
   "/Plan_Lectura_Bilia_365_Dias/index.html",
-  "/Plan_Lectura_Bilia_365_Dias/styles.css",
+  "/Plan_Lectura_Bilia_365_Dias/style.css",
   "/Plan_Lectura_Bilia_365_Dias/script.js",
   "/Plan_Lectura_Bilia_365_Dias/manifest.json",
-  "/Plan_Lectura_Bilia_365_Dias/lecturas.json"
+  "/Plan_Lectura_Bilia_365_Dias/plan_lectura_dinamico.json",
+  "/Plan_Lectura_Bilia_365_Dias/textos_biblicos.json"
 ];
 
 self.addEventListener("install", event => {
